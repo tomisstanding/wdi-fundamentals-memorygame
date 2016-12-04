7002e63b917d800c5439689b707d2621b9602266
thomas <thomas@thomass-MBP.attlocal.net>
2016-12-04 06:28:18 -0600

    added javascript for cards

--- starter-code/main.js
+++ starter-code/main.js
@@ -4,8 +4,29 @@ var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
 
-if (cardTwo === cardFour) {
+/* if (cardTwo === cardFour) {
 	alert("You found a match!")
 } else if {
 	alert("Sorry, try again.")
 } 
+*/
+
+
+
+
+function createCards () {
+ 
+var boardDiv = document.getElementById("game-board");
+ 
+var cards = 4;
+
+for(var i = 0; i < cards; i++){ 
+       var myDiv = document.createElement("div");
+       myDiv.className = "card";
+       boardDiv.appendChild(myDiv);
+  
+   }
+
+}
+
+createCards();
